91400519e602e9a5970ef0711a59c4113f31ea99
Farry Argoebie <fargobie@gmail.com>
2016-10-18 18:28:29 +0700

    moved files into folders

---
+++ batch 3/test.js
@@ -0,0 +1,8 @@
+alert("This is an Alert method");
+confirm("Are you OK?");
+prompt("What is your name?");
+prompt("How old are you?","20");
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed.";
+// }
